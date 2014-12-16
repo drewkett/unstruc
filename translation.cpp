eac643ab18900b01ed3c50966b25314fd8282bf5
Andrew Burkett <burkett.andrew@gmail.com>
2014-12-15 19:28:52 -0800

    Switch to reference

--- translation.cpp
+++ translation.cpp
@@ -10,17 +10,16 @@
 
 void ReadTranslationFile(std::string &filename, TranslationTable &tt) {
 	std::ifstream f;
-	Name name;
 	std::string line, s;
 	std::cerr << "Reading Translation File '" << filename << "'" << std::endl;
 	f.open(filename.c_str(),std::ios::in);
 	if (!f.is_open()) Fatal("Could not open file");
 	while (getline(f,line)) {
-		name = Name();
+		tt.names.resize(tt.names.size() + 1);
+		Name &name = tt.names.back();
 		name.dim = 2;
 		std::istringstream iss(line);
 		iss >> name.name;
-		tt.names.push_back(name);
 		std::cerr << name.name;
 		while (! iss.eof()) {
 			iss >> s;
