df0ba9471f62a901c8a6d404b0966b9d32bdc5ae
Andrew Burkett <burkett.andrew@gmail.com>
2015-02-22 23:09:59 -0800

    Remove a bunch of unnecessary this-> uses

--- src/unstruc/grid.cpp
+++ src/unstruc/grid.cpp
@@ -17,12 +17,12 @@ Grid::Grid(int _dim) : dim(_dim) {
 void Grid::merge_points(double tol) {
 	std::cerr << "Merging Points" << std::endl;
 	int n_merged = 0;
-	int n_points = this->points.size();
+	int n_points = points.size();
 
 	std::cerr << "Sorting Points By Location" << std::endl;
 	std::vector< std::pair<double,int> > s (n_points);
 	for (int i = 0; i < n_points; ++i) {
-		Point& p = this->points[i];
+		Point& p = points[i];
 		s[i] = std::make_pair(p.x+p.y+p.z,i);
 	}
 
@@ -36,14 +36,14 @@ void Grid::merge_points(double tol) {
 	for (int _i = 0; _i < n_points; _i++) {
 		double si = s[_i].first;
 		int i = s[_i].second;
-		Point& p1 = this->points[i];
+		Point& p1 = points[i];
 		for (int _j = _i+1; _j < n_points; _j++) {
 			double sj = s[_j].first;
 			int j = s[_j].second;
 			// Check if point has already been merged
 			if (merged_index[j] != j) continue;
 
-			Point& p2 = this->points[j];
+			Point& p2 = points[j];
 			if (fabs(si - sj) > 3*tol) break;
 			if (same(p1,p2,tol)) {
 				merged_index[j] = i;
@@ -59,33 +59,33 @@ void Grid::merge_points(double tol) {
 	int new_i = 0;
 	for (int i = 0; i < n_points; ++i) {
 		if (merged_index[i] == i) {
-			this->points[new_i] = this->points[i];
+			points[new_i] = points[i];
 			new_index[i] = new_i;
 			new_i++;
 		}
 	}
-	this->points.resize(new_i);
+	points.resize(new_i);
 	for (int i = 0; i < n_points; ++i) {
 		if (merged_index[i] != i)
 			new_index[i] = new_index[merged_index[i]];
 	}
 	std::cerr << n_merged << " Points Merged" << std::endl;
 	std::cerr << "Updating Elements" << std::endl;
-	for (Element& e : this->elements)
+	for (Element& e : elements)
 		for (int& p : e.points)
 			p = new_index[p];
 }
 
 void Grid::delete_inner_faces() {
 	std::cerr << "Deleting Inner Faces" << std::endl;
-	int n_elements = this->elements.size();
-	int n_points = this->points.size();
+	int n_elements = elements.size();
+	int n_points = points.size();
 
 	std::cerr << "Sorting Points By Location" << std::endl;
 	std::vector< std::pair<int,int> > s (n_elements);
 	for (int i = 0; i < n_elements; ++i) {
 		int min_p = n_points;
-		for (int p : this->elements[i].points)
+		for (int p : elements[i].points)
 			if (p < min_p)
 				min_p = p;
 		s[i] = std::make_pair(min_p,i);
@@ -101,13 +101,13 @@ void Grid::delete_inner_faces() {
 	for (int _i = 0; _i < n_elements; _i++) {
 		int si = s[_i].first;
 		int i = s[_i].second;
-		Element &ei = this->elements[i];
-		if (Shape::Info[ei.type].dim == this->dim) continue;
+		Element &ei = elements[i];
+		if (Shape::Info[ei.type].dim == dim) continue;
 		for (int _j = _i+1; _j < n_elements; _j++) {
 			int sj = s[_j].first;
 			int j = s[_j].second;
-			Element &ej = this->elements[j];
-			if (Shape::Info[ej.type].dim == this->dim) continue;
+			Element &ej = elements[j];
+			if (Shape::Info[ej.type].dim == dim) continue;
 			if (deleted_index[j]) continue;
 		    if (si != sj) break;
 			if (same(ei,ej)) {
@@ -122,23 +122,23 @@ void Grid::delete_inner_faces() {
 		if (deleted_index[i]) {
 			n_deleted++;
 		} else {
-			this->elements[new_i] = this->elements[i];
+			elements[new_i] = Element (elements[i]);
 			new_i++;
 		}
 	}
-	this->elements.resize(new_i);
+	elements.resize(new_i);
 	fprintf(stderr,"%d Faces Deleted\n",n_deleted);
 }
 
 void Grid::collapse_elements() {
-	int n_elements = this->elements.size();
+	int n_elements = elements.size();
 
 	std::vector<Element> new_elements;
 	int n_collapsed = 0;
 	int n_deleted = 0;
 	std::vector<bool> deleted_elements (n_elements,false);
 	for (int i = 0; i < n_elements; ++i) {
-		Element& e = this->elements[i];
+		Element& e = elements[i];
 		if (can_collapse(e)) {
 			bool deleted = collapse(e,new_elements);
 			if (deleted) {
@@ -151,14 +151,14 @@ void Grid::collapse_elements() {
 	int new_i = 0;
 	for (int i = 0; i < n_elements; ++i) {
 		if (!deleted_elements[i]) {
-			this->elements[new_i] = this->elements[i];
+			elements[new_i] = elements[i];
 			new_i++;
 		}
 	}
-	this->elements.resize(new_i);
+	elements.resize(new_i);
 
 	int n_added = new_elements.size();
-	this->elements.insert(this->elements.end(),new_elements.begin(),new_elements.end());
+	elements.insert(elements.end(),new_elements.begin(),new_elements.end());
 
 	std::cerr << n_collapsed << " Elements Collapsed" << std::endl;
 	std::cerr << n_added << " Elements Created On Collapse" << std::endl;
